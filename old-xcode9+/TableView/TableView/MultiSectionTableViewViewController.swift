41996070f07abbecb3dab402a0306eada8b5f113
Jusun Kang <wntjs0305@gmail.com>
2020-11-06 22:03:03 +0900

    TableView - MultiSection, Separator

--- old-xcode9+/TableView/TableView/MultiSectionTableViewViewController.swift
+++ old-xcode9+/TableView/TableView/MultiSectionTableViewViewController.swift
@@ -23,16 +23,62 @@
 import UIKit
 
 class MultiSectionTableViewViewController: UIViewController {
+    
+    let list = PhotosSettingSection.generateData()
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
       
    }
+    
+    @objc func switchChanged(_ sender: UISwitch) {
+        print(sender.isOn, sender.tag)
+    }
 }
 
 
-
+extension MultiSectionTableViewViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list[section].items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let target = list[indexPath.section].items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: target.type.rawValue, for: indexPath)
+        
+        switch target.type {
+        case .detailTitle:
+            cell.textLabel?.text = target.title
+            cell.detailTextLabel?.text = target.subTitle
+            cell.imageView?.image = UIImage(named: target.imageName!)
+        case .switch:
+            cell.textLabel?.text = target.title
+            if let switchView = cell.accessoryView as? UISwitch {
+                switchView.isOn = target.on
+                switchView.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+                switchView.tag = indexPath.section
+            }
+        case .checkmark:
+            cell.textLabel?.text = target.title
+            cell.accessoryType = target.on ? .checkmark : .none
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return list[section].header
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return list[section].footer
+    }
+}
 
 
 
---
+++ old-xcode9+/TableView/TableView/SwitchTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SwitchTableViewCell.swift
+//  TableView
+//
+//  Created by JuSun Kang on 2020/11/06.
+//  Copyright © 2020 Keun young Kim. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        let v = UISwitch(frame: .zero)
+        accessoryView = v
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
